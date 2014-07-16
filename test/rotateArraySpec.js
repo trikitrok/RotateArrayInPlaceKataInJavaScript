ac687c7960933650a86989f2ae2962c527f98635
trikitrok <trikitrok@gmail.com>
2014-07-17 00:34:01 +0200

    A non empty array rotated by 0 steps does not change

--- test/rotateArraySpec.js
+++ test/rotateArraySpec.js
@@ -12,4 +12,14 @@ describe("Array rotation in place", function() {
     expect(resultingArray).toBe(originalArray);
     expect(resultingArray).toEqual([]);
   });
+
+  it("A non empty array rotated by 0 steps does not change", function() {
+    var originalArray = [1, 2],
+      resultingArray = ArrayRotation.rotateInPlace(
+        originalArray, 0
+      );
+
+    expect(resultingArray).toBe(originalArray);
+    expect(resultingArray).toEqual([1, 2]);
+  });
 });
