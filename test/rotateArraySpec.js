c09bed8dba9464d22da07359abc83551bf118adb
trikitrok <trikitrok@gmail.com>
2014-07-17 01:00:24 +0200

    Refactor: removed overlapping tests and renamed the remaining one

--- test/rotateArraySpec.js
+++ test/rotateArraySpec.js
@@ -23,37 +23,7 @@ describe("Array rotation in place", function() {
     expect(resultingArray).toEqual([1, 2]);
   });
 
-  it("A rotation by 1 step changes an array of size 2", function() {
-    var originalArray = [1, 2],
-      resultingArray = ArrayRotation.rotateInPlace(
-        originalArray, 1
-      );
-
-    expect(resultingArray).toBe(originalArray);
-    expect(resultingArray).toEqual([2, 1]);
-  });
-
-  it("A rotation by 1 step changes an array of size 3", function() {
-    var originalArray = [1, 2, 3],
-      resultingArray = ArrayRotation.rotateInPlace(
-        originalArray, 1
-      );
-
-    expect(resultingArray).toBe(originalArray);
-    expect(resultingArray).toEqual([3, 1, 2]);
-  });
-
-  it("A rotation by 1 step changes an array of size 4", function() {
-    var originalArray = [1, 2, 3, 4],
-      resultingArray = ArrayRotation.rotateInPlace(
-        originalArray, 1
-      );
-
-    expect(resultingArray).toBe(originalArray);
-    expect(resultingArray).toEqual([4, 1, 2, 3]);
-  });
-
-  it("A rotation by 2 steps changes an array of size 4", function() {
+  it("A rotation by a number of steps that is less than the array size", function() {
     var originalArray = [1, 2, 3, 4],
       resultingArray = ArrayRotation.rotateInPlace(
         originalArray, 2
@@ -62,6 +32,4 @@ describe("Array rotation in place", function() {
     expect(resultingArray).toBe(originalArray);
     expect(resultingArray).toEqual([3, 4, 1, 2]);
   });
-
-
 });
